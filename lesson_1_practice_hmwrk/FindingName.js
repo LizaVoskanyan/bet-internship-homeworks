f5e285d74c12b3968b0615d091ec22146e3de451
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-15 09:26:33 -0700

    Update FindingName.js

--- lesson_1_practice_hmwrk/FindingName.js
+++ lesson_1_practice_hmwrk/FindingName.js
@@ -1,8 +1,13 @@
 
-function findingName(string){
-	var str = string; 
-	var reg = /({\w+})/i;
-	var found = str.match(reg);
-	return found[1];
+function findingName(str){
+  
+	var obj = '[object ';
+	var len = obj.length;
+	var strLen = str.length;
+	if(str.includes(obj)){
+	  return  str.substring( len - 1 , strLen -1  );
+	  }   
+	else   { return "somthing goes wrong"; }
 }
-findingName('[someObject ${anun}]');
+console.log(findingName('[someobject anun]'));
+console.log(findingName('[object anun]'));
