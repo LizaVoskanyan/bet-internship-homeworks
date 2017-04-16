8246bff21c6f0dd6045080605118cd16de08596f
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-16 00:02:44 -0700

    Update forHoisting.js

--- lesson_1_practice_hmwrk/forHoisting.js
+++ lesson_1_practice_hmwrk/forHoisting.js
@@ -1,22 +1,12 @@
 (function() {
- var calculator
- function fib(n)
-function arithmeticalMean(n)
-
-calculator = { addition : function(a,b) { return a + b ;},
-               multiplication : function(a,b) { return a * b ;},
-               subtraction : function(a,b) { return a - b;},
-               division : function (a,b) { return a/b;}       // i don't know about hoisting in objects yet
-    };
-    
-     fib(n) {
-      return n <= 1 ? n :calculator.addition( fib(n - 1) , fib(n - 2) );
-    }
-  
-    arithmeticalMean(n){
-        var sum = 0;
-        var i 
-        var arithmeticalMean
+ var calculator;
+ function fib(n){
+   return n <= 1 ? n :calculator.addition( fib(n - 1) , fib(n - 2) );
+  }
+ function arithmeticalMean(n){
+        var sum ;
+        var i;
+        var arithmeticalMean;
 
         sum = 0;
         for(i = 0 ; i <= n ; i++ ){  
@@ -25,9 +15,12 @@ calculator = { addition : function(a,b) { return a + b ;},
         arithmeticalMean = calculator.division(sum , n);
         return arithmeticalMean;
     }
-    
-      
-    
-   console.log(arithmeticalMean(5));
+
+calculator = { addition : function(a,b) { return a + b ;},
+               multiplication : function(a,b) { return a * b ;},
+               subtraction : function(a,b) { return a - b;},
+               division : function (a,b) { return a/b;}       // i don't know about hoisting in objects yet
+    };
+    console.log(arithmeticalMean(5));
    
   })(); 
