b6330ef4d6d9ce5fe0666dc8059f793ef4a85c9e
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-15 23:51:25 -0700

    Update toBool.js

--- lesson_1_practice_hmwrk/toBool.js
+++ lesson_1_practice_hmwrk/toBool.js
@@ -1,5 +1,4 @@
 function toBool(str){
-    if( str == 'true'){ return true;    }
-    else if(str == 'false' ){   return fasle ;  }
-    else { return "i don't know what to du in this case";}
+     if (str === "false" || str === "NaN" || str === "null"  || str === "undefined" || str === "0" ){return false ;}
+     else {return !!str;}
 }
