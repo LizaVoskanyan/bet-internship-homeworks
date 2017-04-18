64bf17438ac1f4cdf5ddda41951550a5d03cc1a3
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-18 13:12:02 -0700

    Update FindingName.js

--- lesson_1_practice_hmwrk/FindingName.js
+++ lesson_1_practice_hmwrk/FindingName.js
@@ -1,10 +1,9 @@
-
-function findingName(str){
-  
-	if(str.includes('[object ')){
-	  return  str.substring( 8 , str.length -1  );
-	  }   
-	else   { return "somthing goes wrong"; }
+function findingName(str) {
+    if (str.includes('[object ')) {
+        return str.substring(8, str.length - 1);
+    } else {
+        return "somthing goes wrong";
+    }
 }
 console.log(findingName('[someobject anun]'));
 console.log(findingName('[object anun]'));
