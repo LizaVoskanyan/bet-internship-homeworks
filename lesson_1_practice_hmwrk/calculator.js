9ce1108dafffca23b4ea47fe22ad4b5ae6846af4
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-18 13:12:36 -0700

    Update calculator.js

--- lesson_1_practice_hmwrk/calculator.js
+++ lesson_1_practice_hmwrk/calculator.js
@@ -1,26 +1,35 @@
 (function() {
-    var calculator = { addition : function(a,b) { return a + b ;},
-      multiplication : function(a,b) { return a * b ;},
-      subtraction : function(a,b) { return a - b;},
-      division : function (a,b) { return a/b;}
-    };
-    
-    function fib(n) {
-      return n <= 1 ? n :calculator.addition( fib(n - 1) , fib(n - 2) );
-    }
-  
-    function arithmeticalMean(n){
-        var sum = 0;
-        
-        for(var i = 0 ; i <= n ; i++ ){  
-          sum = calculator.addition( sum , fib(i));
+    var calculator = {
+        addition: function(a, b) {
+            return a + b;
+        },
+        multiplication: function(a, b) {
+            return a * b;
+        },
+        subtraction: function(a, b) {
+            return a - b;
+        },
+        division: function(a, b) {
+            return a / b;
         }
-        var arithmeticalMean= calculator.division(sum , n);
+    };
+
+    function fib(n) {
+        return n <= 1 ? n : calculator.addition(fib(n - 1), fib(n - 2));
+    }
+
+    function arithmeticalMean(n) {
+        var sum = 0;
+
+        for (var i = 0; i <= n; i++) {
+            sum = calculator.addition(sum, fib(i));
+        }
+        var arithmeticalMean = calculator.division(sum, n);
         return arithmeticalMean;
     }
-    
-      
-    
-   console.log(arithmeticalMean(5));
-   
-  })(); 
+
+
+
+    console.log(arithmeticalMean(5));
+
+})();
