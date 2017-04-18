4c84b085edafb69e29947be777beae5e387f71a3
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-18 13:16:17 -0700

    Update toBool.js

--- lesson_1_practice_hmwrk/toBool.js
+++ lesson_1_practice_hmwrk/toBool.js
@@ -1,4 +1,7 @@
-function toBool(str){
-     if (str === "false" || str === "NaN" || str === "null"  || str === "undefined" || str === "0" ){return false ;}
-     else {return !!str;}
+function toBool(str) {
+    if (str === "false" || str === "NaN" || str === "null" || str === "undefined" || str === "0") {
+        return false;
+    } else {
+        return !!str;
+    }
 }
