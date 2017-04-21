0045a52830b5421206a2181b6b27e1cb43d4ce7d
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-21 00:42:04 -0700

    Update randomColor.js

--- lesson_1_practice_hmwrk/randomColor.js
+++ lesson_1_practice_hmwrk/randomColor.js
@@ -1,9 +1,7 @@
 function randomColor() {
     function s4() { 
-
         return  Math.floor((1 + Math.random()) * 0x10000).toString(16)
         .substring(2);
     }
      return '#'+s4() + s4() ;
 }
-randomColor();
