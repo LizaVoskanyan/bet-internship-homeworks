db00f9cbab95a5abd73988f10ae713f32a5874de
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-22 13:10:39 -0700

    Update matrixLastIndexOf.js

--- lesson_3_practice_hmwrk/matrixLastIndexOf.js
+++ lesson_3_practice_hmwrk/matrixLastIndexOf.js
@@ -1,16 +1,16 @@
 (function () {
     'use strict';
     function matrixLastIndexOf(matrix, item) {
-        var a={};
-        for(var i = matrix.length - 1 ; i >= 0; i--) {
-            for(var j = matrix[i].length -1 ; j >= 0; j--) {
+        var a = {};
+        for(var i = matrix.length - 1; i >= 0; i--) {
+            for(var j = matrix[i].length -1; j >= 0; j--) {
                 if(matrix[i][j] === item) {
                     a.i = i + 1;
                     a.j = j + 1;
                     return a;
                 }
             }
-         }
-         return -1;
+        }
+        return -1;
     }
 })();
