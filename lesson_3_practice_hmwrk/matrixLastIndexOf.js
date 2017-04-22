a00b097b06f783e4baf391c1c30ffc18d2196910
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-22 13:12:34 -0700

    Update matrixLastIndexOf.js

--- lesson_3_practice_hmwrk/matrixLastIndexOf.js
+++ lesson_3_practice_hmwrk/matrixLastIndexOf.js
@@ -3,7 +3,7 @@
     function matrixLastIndexOf(matrix, item) {
         var a = {};
         for(var i = matrix.length - 1; i >= 0; i--) {
-            for(var j = matrix[i].length -1; j >= 0; j--) {
+            for(var j = matrix[i].length - 1; j >= 0; j--) {
                 if(matrix[i][j] === item) {
                     a.i = i + 1;
                     a.j = j + 1;
