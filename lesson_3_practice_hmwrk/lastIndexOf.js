9d79d0318547ccd501debbae8a6335bd1ee918cf
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-22 13:12:12 -0700

    Update lastIndexOf.js

--- lesson_3_practice_hmwrk/lastIndexOf.js
+++ lesson_3_practice_hmwrk/lastIndexOf.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     function lastIndexOf(array, item) {
-        for(var i = array.length; i >= 0; i--) {
+        for(var i = array.length - 1; i >= 0; i--) {
             if(array[i] === item) {
                 return i;
             }
