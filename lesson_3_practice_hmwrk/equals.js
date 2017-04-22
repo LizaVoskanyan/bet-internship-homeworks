e5237c624bf9ed81af3cd8c0df888fd304994d57
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-22 12:30:39 -0700

    Update equals.js

--- lesson_3_practice_hmwrk/equals.js
+++ lesson_3_practice_hmwrk/equals.js
@@ -1,19 +1,19 @@
 (function () {
     'use strict';
     function equals(arg1, arg2) {
-        if(arg1 == arg2) {
+        if(arg1 === arg2) {
             return true;
         }
         for(var i in arg1) {
-            if(arg1[i] !== arg2[i]) {
+            if(!equals(arg1[i], arg2[i])) {
                 return false;
             }
         }
         for(var i in arg2) {
-            if(arg1[i] !== arg2[i]) {
+            if(!equals(arg1[i], arg2[i])) {
                 return false;
             }
+            return true;
         }
-        return true;
     }
 })();
