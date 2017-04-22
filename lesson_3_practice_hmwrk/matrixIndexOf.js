865c7053eb6eded6ae20cd046a3b1fe00fae80b8
LizaVoskanyan <voskanyan.liza.lv@gmail.com>
2017-04-22 13:00:17 -0700

    Update matrixIndexOf.js

--- lesson_3_practice_hmwrk/matrixIndexOf.js
+++ lesson_3_practice_hmwrk/matrixIndexOf.js
@@ -1,16 +1,16 @@
 (function () {
     'use strict';
     function matrixIndexOf(matrix, item) {
-        var a = {};
         for(var i = 0; i < matrix.length; i++) {
             for(var j = 0; j < matrix[i].length; j++) {
                 if(matrix[i][j] == item) {
-                    a.i = i + 1;
-                    a.j = j + 1;
-                    return a;
+                    return {
+                        i: i,
+                        j: j
+                    };
                 }
             }
         }
-        return -1;
+        return null;
     }
 })();
